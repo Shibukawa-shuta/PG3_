75bf8884f5be5ad287770813471e37f6be0b7e88
Shibukawa_shuta <115063018+Shibukawa-shuta@users.noreply.github.com>
2024-02-09 12:45:12 +0900

    06_02

--- main.cpp
+++ main.cpp
@@ -1,24 +1,35 @@
-#include <stdio.h>
-#include "Animal.h"
-#include "Dog.h"
-#include "Cat.h"
+#include <iostream>
+#include <thread>
+#include <Windows.h>
 
+void Quadruple(int number) {
+	number *= 4;
+	std::cout << "thread 1 : " << number << std::endl;
+}
 
-int main() {
+void AddFour(int number) {
+	number += 4;
+	std::cout << "thread 2 : " << number << std::endl;
+}
 
-	Animal* animal_[2] ;
-	for (int i = 0; i < 2; i++){
-		if (i < 1) {
-			animal_[i] = new Dog;
-		}
-		else {
-			animal_[i] = new Cat;
-		}
-	}
+void SubtractFour(int number) {
+	number -= 4;
+	std::cout << "thread 3 : " << number << std::endl;
+}
 
-	for (int i = 0; i < 2; i++) {
-		animal_[i]->Cry();
-	}
+int main(void) {
+	SetConsoleOutputCP(65001);
+
+	int number = 4;
+
+	std::thread th1(Quadruple, number);
+	th1.join();
+
+	std::thread th2(AddFour, number);
+	th2.join();
+
+	std::thread th3(SubtractFour, number);
+	th3.join();
 
 	return 0;
 }
