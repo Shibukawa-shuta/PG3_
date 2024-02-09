f12398942ca74b11607f022ebb1ace9025e5b57e
Shibukawa_shuta <115063018+Shibukawa-shuta@users.noreply.github.com>
2024-02-09 12:31:48 +0900

    05_01

--- main.cpp
+++ main.cpp
@@ -1,23 +1,70 @@
 #include <stdio.h>
-#include "Animal.h"
-#include "Dog.h"
-#include "Cat.h"
+#include <iostream>
+#include <Windows.h>
+#include <list>
 
+int main(void) {
+	SetConsoleOutputCP(65001);
 
-int main() {
+	std::list<const char*> yamanoteStationName = {
+		"Tokyo",
+		"Kanda",
+		"Akihabara",
+		"Okachimachi",
+		"Ueno",
+		"Uguisudani",
+		"Nippori",
+		"Tabata",
+		"Komagome",
+		"Sugamo",
+		"Otsuka",
+		"Ikebukuro",
+		"Mejiro",
+		"Takadanobaba",
+		"Shin-Okubo",
+		"Shinjuku",
+		"Yoyogi",
+		"Harajuku",
+		"Shibuya",
+		"Ebisu",
+		"Meguro",
+		"Gotanda",
+		"Osaki",
+		"Shinagawa",
+		"Tamachi",
+		"Hamamatsucho",
+		"Shimbashi",
+		"Yurakucho",
+	};
 
-	Animal* animal_[2] ;
-	for (int i = 0; i < 2; i++){
-		if (i < 1) {
-			animal_[i] = new Dog;
-		}
-		else {
-			animal_[i] = new Cat;
-		}
+	std::cout << "1970”N‚ÌŽRŽèü‰wˆê——" << std::endl;
+
+	for (auto& name : yamanoteStationName) {
+		std::cout << name << std::endl;
 	}
 
-	for (int i = 0; i < 2; i++) {
-		animal_[i]->Cry();
+	std::cout << "\n" << std::endl;
+
+	auto findStationNameIterator = std::find(yamanoteStationName.begin(), yamanoteStationName.end(), "Nippori");
+	findStationNameIterator++;
+	yamanoteStationName.insert(findStationNameIterator, "Nishi-Nippori");
+
+	std::cout << "2019”N‚ÌŽRŽèü‰wˆê——" << std::endl;
+
+	for (auto& name : yamanoteStationName) {
+		std::cout << name << std::endl;
+	}
+
+	std::cout << "\n" << std::endl;
+
+	findStationNameIterator = std::find(yamanoteStationName.begin(), yamanoteStationName.end(), "Shinagawa");
+	findStationNameIterator++;
+	findStationNameIterator = yamanoteStationName.insert(findStationNameIterator, "Takanawa Gateway");
+
+	std::cout << "2022”N‚ÌŽRŽèü‰wˆê——" << std::endl;
+
+	for (auto& name : yamanoteStationName) {
+		std::cout << name << std::endl;
 	}
 
 	return 0;
